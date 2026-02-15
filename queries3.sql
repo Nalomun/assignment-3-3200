73349532ce7a9063a46838ef4df9468737e273ee
Nalomun <105822335+Nalomun@users.noreply.github.com>
2026-02-14 16:05:11 -0800

    BLAHG BLAEH BLEHHH BAAAH

--- queries3.sql
+++ queries3.sql
@@ -1,3 +1,5 @@
+DROP TABLE if exists MusicVideo;
+
 --Query1
 CREATE TABLE MusicVideo (
     TrackID INTEGER PRIMARY KEY,
@@ -17,15 +19,12 @@ INSERT INTO MusicVideo (TrackID, VideoDirector) VALUES (8, 'Bush Jr');
 INSERT INTO MusicVideo (TrackID, VideoDirector) VALUES (9, 'Teddy');
 INSERT INTO MusicVideo (TrackID, VideoDirector) VALUES (10, 'BAH');
 
-
---Test query
-SELECT TrackID, Name FROM tracks LIMIT 20;
-
 --Query3
 INSERT INTO MusicVideo (TrackID, VideoDirector)
 SELECT TrackID, 'Joe Biden'
 FROM tracks
-WHERE Name = 'Voodoo';
+WHERE Name = 'Voodoo'
+LIMIT 1;
 
 --Query4
 SELECT Name
@@ -51,3 +50,29 @@ FROM tracks
 JOIN genres ON tracks.GenreId = genres.GenreId
 GROUP BY genres.Name
 ORDER BY NumberOfTracks DESC;
+
+-- Query 7:
+SELECT DISTINCT c.CustomerId, c.FirstName, c.LastName
+FROM customers c
+JOIN invoices i ON c.CustomerId = i.CustomerId
+JOIN invoice_items ii ON i.InvoiceId = ii.InvoiceId
+JOIN tracks t ON ii.TrackId = t.TrackId
+WHERE t.Milliseconds > (
+    SELECT AVG(Milliseconds)
+    FROM tracks
+    WHERE Milliseconds <= 900000
+)
+AND t.Milliseconds <= 900000;
+
+-- Query 8:
+SELECT t.TrackId, t.Name, g.Name AS GenreName
+FROM tracks t
+JOIN genres g ON t.GenreId = g.GenreId
+WHERE t.GenreId NOT IN (
+    SELECT GenreId
+    FROM tracks
+    GROUP BY GenreId
+    ORDER BY SUM(Milliseconds) DESC
+    LIMIT 5
+);
+
